dafd5df1d09adbdd5bb4332b918b50332e365508
kkang2u <kkang@instructors.2u.com>
2025-03-20 20:33:20 -0500

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
+## uvicorn main:app --reload
+
+from fastapi import FastAPI, Depends, HTTPException, Request
 import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,12 +8,14 @@ from fastapi.middleware.cors import CORSMiddleware
 # Load environment variables
 #load_dotenv()
 
+allow_url=["http://localhost:4200"]
+
 app = FastAPI()
 
 # Allow Angular frontend to communicate with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Adjust based on frontend URL
+    allow_origins=allow_url,  # Adjust based on frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,6 +32,9 @@ def get_api_key():
     return API_KEY
 
 @app.get("/secure-data")
-def get_secure_data(api_key: str = Depends(get_api_key)):
-    """Secure endpoint that requires API key"""
+def get_secure_data(request: Request, api_key: str = Depends(get_api_key)):
+    """Secure endpoint that requires API key and checks request origin"""
+    origin = request.headers.get("origin")
+    if origin not in allow_url:
+        raise HTTPException(status_code=403, detail=f"Access denied: {origin} is not an allowed domain")
     return {"message": "This is secure data", "api_key": api_key}
